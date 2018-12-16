6be669a4acc34ba9edc4c1e181698f41669b467e
N.TAHAR <najibtaharberrabah@gmail.com>
2018-12-16 23:15:43 +0100

    experiencePage MobileFirst Iphone5 slick test

---
+++ src/js/experience.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+  $('.experiencePage__experiences').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
+	
